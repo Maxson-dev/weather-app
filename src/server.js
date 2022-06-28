2dfc24a41958052086fd3c48f75ec5b0489ebb69
Maxson-dev <maxim.shnyagin@yandex.ru>
2022-06-28 15:03:26 +0300

    add backend

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const PORT = 3000;
+
+async function createServer() {
+  const app = express();
+  app.use(express.static("build"));
+  app.use("*", async (req, res) => {
+    try {
+      const html = await fs.readFile("./index.html", "utf-8");
+      res.setHeader("Content-Type", "text/html");
+      res.end(html);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).end(err);
+    }
+  })
+  return app;
+}
+
+createServer().then( app => app.listen( PORT, () => {
+  console.log(`Server is listening on port ${3000}`);
+}) )
